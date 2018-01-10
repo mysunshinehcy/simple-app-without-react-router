ea0ca5da7ff6eda62e8c28c23e96165b6dd842f8
hfpp2012 yinsigan <ganweiliang886@gmail.com>
2018-01-10 11:39:40 +0800

    initial

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+import Home from './pages/Home';
+// import About from './pages/About.jsx';
+// import Contact from './pages/Contact.jsx';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Home />
+      </div>
+    );
+  }
+}
+
+export default App;
